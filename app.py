e576bde5c25053ecbbc64d39767aaf1db7765f9f
pourhossinm <pourhossinm@gmail.com>
2025-02-02 18:04:55 +0330

    app.py upadate 2

--- app.py
+++ app.py
@@ -26,6 +26,6 @@ def handle_candidate(data):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # مقدار پیش‌فرض 5000 است، اگر PORT موجود نباشد
     socketio.run(app, debug=True)
-    app.run(host='0.0.0.0', port=port)
+    # app.run(host='0.0.0.0', port=port)
     # app.run(debug=True)
 
