0e144ce15374428535b7250e63e9de25ab2a5bf8
pourhossinm <pourhossinm@gmail.com>
2025-02-03 19:18:42 +0330

    update app.py27

--- app.py
+++ app.py
@@ -50,20 +50,48 @@
 
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import os
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+clients = {}
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('message')
-def handle_message(message):
-    print('received message: ' + message)
-    emit('message', message, broadcast=True)
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    clients[username] = room
+    join_room(room)
+    emit('message', {'message': f'{username} has entered the room.'}, room=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    del clients[username]
+    emit('message', {'message': f'{username} has left the room.'}, room=room)
+
+@socketio.on('offer')
+def handle_offer(data):
+    emit('offer', data, room=data['room'])
+
+@socketio.on('answer')
+def handle_answer(data):
+    emit('answer', data, room=data['room'])
+
+@socketio.on('candidate')
+def handle_candidate(data):
+    emit('candidate', data, room=data['room'])
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
