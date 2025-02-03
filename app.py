05bd7573a33a63d488004209b9c8a06722cc1f65
pourhossinm <pourhossinm@gmail.com>
2025-02-03 19:33:05 +0330

    update app.py 222

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def handle_candidate(data):
 
 if __name__ == '__main__':
     # تغییر پورت به پورت سیستم عامل
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     socketio.run(app, host='0.0.0.0', port=port)
