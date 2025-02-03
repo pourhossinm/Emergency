8a9ba7de2aa59f581ae9f88c77d0d22c3b424684
pourhossinm <pourhossinm@gmail.com>
2025-02-03 19:29:53 +0330

    update app.py28

--- app.py
+++ app.py
@@ -1,54 +1,3 @@
-# from flask import Flask, render_template, request
-# from flask_socketio import SocketIO, emit
-# import os
-#
-# app = Flask(__name__)
-# socketio = SocketIO(app, cors_allowed_origins="*")
-#
-# users = []  # لیست کاربران متصل
-#
-# @app.route("/")
-# def index():
-#     return render_template("index.html")
-#
-# @socketio.on("join")
-# def handle_join():
-#     try:
-#         sid = request.sid
-#         users.append(sid)
-#
-#         print(f"User {sid} joined. Total users: {len(users)}")  # مشاهده لاگ دقیق
-#
-#         if len(users) == 2:
-#             emit("start_call", {"peer": users[1]}, room=users[0])
-#             emit("start_call", {"peer": users[0]}, room=users[1])
-#     except Exception as e:
-#         print(f"Error in join event: {e}")  # چاپ خطای دقیق
-#
-# @socketio.on("offer")
-# def handle_offer(data):
-#     emit("offer", data, broadcast=True)
-#
-# @socketio.on("answer")
-# def handle_answer(data):
-#     emit("answer", data, broadcast=True)
-#
-# @socketio.on("candidate")
-# def handle_candidate(data):
-#     emit("candidate", data, broadcast=True)
-#
-# @socketio.on("disconnect")
-# def handle_disconnect():
-#     sid = request.sid
-#     if sid in users:
-#         users.remove(sid)
-#     print(f"User {sid} disconnected. Remaining users: {len(users)}")
-#
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 8080))
-#     socketio.run(app, host="0.0.0.0", port=port)
-
-
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room, leave_room
 import os
@@ -91,8 +40,6 @@ def handle_candidate(data):
     emit('candidate', data, room=data['room'])
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
-
-if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 8080))
-    socketio.run(app, host="0.0.0.0")
+    # تغییر پورت به پورت سیستم عامل
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
