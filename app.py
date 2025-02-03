3dc55335e1c914a1f3521e70d6a6ad255aaf2738
pourhossinm <pourhossinm@gmail.com>
2025-02-03 19:23:40 +0330

    update app.py27

--- app.py
+++ app.py
@@ -94,5 +94,5 @@ if __name__ == '__main__':
     socketio.run(app, debug=True)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
-    socketio.run(app, host="0.0.0.0", port=port)
+    # port = int(os.environ.get("PORT", 8080))
+    socketio.run(app, host="0.0.0.0")
