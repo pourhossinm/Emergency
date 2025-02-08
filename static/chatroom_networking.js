e113f5ee6c7b5859956755f18425c08e5bdf3fef
pourhossinm <pourhossinm@gmail.com>
2025-02-08 16:43:06 +0330

    update 20-9

--- static/chatroom_networking.js
+++ static/chatroom_networking.js
@@ -45,7 +45,7 @@ socket.on("connect", ()=>{
 
 socket.on("user-connect", (data)=>{
     console.log("user-connect ", data);
-    alert(data["sid"]);
+    alert(data["sid"] + data["name"]);
     let peer_id = data["sid"];
     let display_name = data["name"];
 
